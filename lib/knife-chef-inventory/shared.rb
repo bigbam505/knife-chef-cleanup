40a4d8ac7e3078f3053fbbedcaf6e8bf4e94d3f3
Brent Montague <brent@bmontague.com>
2016-08-19 00:28:02 -0400

    Improve performance by switching to parial search (#14)
    This updates the chef node searches to be partial searches which should
    help serialization and memory usage locally.

---
+++ lib/knife-chef-inventory/shared.rb
@@ -0,0 +1,15 @@
+module KnifeChefInventory
+  module Shared
+    def time_since(timestamp)
+      (Time.now - Time.at(timestamp)).to_i / 60
+    end
+
+    def search_nodes(query)
+      Chef::Search::Query.new.search(:node, query, search_args).first
+    end
+
+    def max_results
+      Chef::Node.list.count || 1000
+    end
+  end
+end
